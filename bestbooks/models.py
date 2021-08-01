84e0ee5d8f1cf0197577f277002cb75d04a32c60
songo-ku <karczewski49@gmail.com>
2021-08-01 19:52:12 +0200

    many changes, igonres, sqlite, venv, requirements, migrations, models, views

--- bestbooks/models.py
+++ bestbooks/models.py
@@ -2,18 +2,38 @@ from django.db import models
 from django.utils import timezone
 import datetime
 
+
 class Author(models.Model):
-    author_fl = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
-    # books =
 
     def __str__(self):
-        return self.author_fl
+        return f'{self.first_name} {self.last_name}'
 
     def was_published_recently(self):
         return self.created >= timezone.now() - datetime.timedelta(days=1)
 
 #      should be here method to response all books per author
+
+
+# class AuthorDescription:
+#     # napisać sygnał gdy dodaje kolejna ksiazke dla autora zeby weryfikował czy books written sie nie powinno zmienić
+#     BOOKS_WRITTEN = (
+#         ('s', 'less than 5 books'),
+#         ('m', 'less or equal 30'),
+#         ('h', 'more than 30'),
+#     )
+#     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+#     birthday = models.DateField(null=True, blank=True)
+#     books_written = models.CharField(max_length=1, choices=BOOKS_WRITTEN)
+#     youtube_url = models.URLField(null=True, blank=True)
+#
+#     def calculate_age(self):
+#         import datetime
+#         return int((datetime.datetime.now() - self.birthday).days / 365.25)
+#     age = property(calculate_age)
+#     # < span > Age: {{person.age | timesince}} < / span > ciekawostka zeby nie trzeba bylo robic tutaj obliczen
 
 
 class Book(models.Model):
@@ -26,3 +46,4 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
--- bestbooks/views.py
+++ bestbooks/views.py
@@ -1,6 +1,54 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
 from .models import Author, Book
+
+
+class IndexView(generic.ListView):
+    template_name = 'bestbooks/index.html'
+    context_object_name = 'authors_lists'
+
+    def get_queryset(self):
+        return Author.objects.all()
+
+
+class DetailView(generic.DetailView):
+    model = Author
+    template_name = 'bestbooks/detail.html'
+    # Book.objects.filter(pub_date__lte=timezone.now()).exclude(
+    #     choice__choice_text__isnull=True).order_by('-pub_date')[:10]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print(context['object'])
+        author = context['object']
+        print(author.id)
+        print(Book.objects.filter(author__id=author.id))
+        context['books'] = Book.objects.filter(author__id=author.id)
+        return context
+    # author.id
+    # self.get_object()
+
+    # def get_queryset(self):
+    #     return Author.objects.filter(id=pk)
+
+
+def results(request, question_id):
+    response = 'you are looking at the results of question %s.'
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse('you are voting on question %s.' % question_id)
+
+
+def authors_list(request):
+    authorsList = Author.objects.all()
+    return render(request, 'bestbooks/authors_list.html', {'authorsList': authorsList})
+
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[0:5]
@@ -9,18 +57,8 @@ def index(request):
     }
     return render(request, 'polls/index.html', context)
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    print(f'wyswietl question {question}')
-    return render(request, 'polls/detail.html', {'question': question})
 
-def results(request, question_id):
-    response = 'you are looking at the results of question %s.'
-    return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-    return HttpResponse('you are voting on question %s.' % question_id)
-
-def authors_list(request):
-    authorsList = Author.objects.all()
-    return render(request, 'bestbooks/authors_list.html', {'authorsList': authorsList})
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     print(f'wyswietl question {question}')
+#     return render(request, 'polls/detail.html', {'question': question})
